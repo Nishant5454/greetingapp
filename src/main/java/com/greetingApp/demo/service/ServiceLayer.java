2967e9b963d4abb3e1b0903399f388c4157f18de
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-23 03:16:08 +0530

    [Nishant]Add.Get message feature on the basis of first and last name

--- src/main/java/com/greetingApp/demo/service/ServiceLayer.java
+++ src/main/java/com/greetingApp/demo/service/ServiceLayer.java
@@ -8,4 +8,17 @@ public class ServiceLayer {
     public String getMeesage(){
         return "Hello from BridgeLabz";
     }
+    public String getNameviaFirst(String firstName){
+        return "Hello"+" "+firstName;
+    }
+    public String getNameviaLast(String lastName){
+        return "Hello"+" "+lastName;
+    }
+    public String getNameviafirstLast(String firstName,String lastName){
+        return "Hello"+" "+firstName+"  "+lastName;
+    }
+
+
+
+
 }
