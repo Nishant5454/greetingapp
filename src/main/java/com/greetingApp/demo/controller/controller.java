affb5acade81005a58c11368b1d8ec3269f2a78a
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-23 01:34:32 +0530

    [Nishant]Add.service method

--- src/main/java/com/greetingApp/demo/controller/controller.java
+++ src/main/java/com/greetingApp/demo/controller/controller.java
@@ -2,6 +2,8 @@ package com.greetingApp.demo.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greetingApp.demo.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -10,10 +12,14 @@ import java.util.Map;
 
 public class controller {
    private final ObjectMapper objectMapper=new ObjectMapper();
+
+    public controller(ServiceLayer service) {
+        this.service = service;
+    }
+
     @GetMapping("/user")// Get method to json
     public String getUser() throws JsonProcessingException {
-
-        return "Hello"+ objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(name);
+        return "Hello User";
     }
     @PostMapping("/user/post")
     public String postUser(@RequestBody Greeting greeting) throws JsonProcessingException {
@@ -32,5 +38,15 @@ public class controller {
         //name=name.trim();
         return objectMapper.writeValueAsString(greeting.getName())+" is deleted from the Database";
     }
+    private ServiceLayer service;
+    @Autowired
+    public void GreetingController(ServiceLayer service){
+        this.service=service;
+    }
+    @GetMapping("/service")
+    public String getGreeting(){
+        return service.getMeesage();
+    }
+
 
 }
